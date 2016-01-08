8e96201562837af462d87d141e35c61afe835d82
Julien Delage <julien.delage@orange.com>
2016-01-08 12:01:57 +0100

    chore: auto publish to gh-pages

---
+++ deploy-gh-pages.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Ensure script exec in project root
+cd "`dirname \"$0\"`" && cd ..
+
+cd dist
+git init
+git config user.name "webcomOps"
+git config user.email "webcom.ops@orange.com"
+git add .
+git commit -m "Deploy to GitHub Pages"
+git push --force --quiet "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" master:gh-pages > /dev/null 2>&1
