7db9ac29940ddb4a3cf95b631e44728fa630623b
Paul Musso <paul.musso@orange.com>
2016-05-09 09:42:08 +0200

    chore: add constant to build production bundle bis

--- webpack.config.js
+++ webpack.config.js
@@ -62,7 +62,9 @@ const config = {
 			__WEBCOM_SERVER__: JSON.stringify(process.env.WS_SERVER || 'https://webcom.orange.com'),
 			__NAMESPACE__: JSON.stringify(process.env.NAMESPACE || 'visiowebrtc'),
 			__DEVTOOLS__: process.env.NODE_ENV !== 'production',
-			NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+			'process.env': {
+				NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+			}
 		})
 	],				
 	progress: true,
